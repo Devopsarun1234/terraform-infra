ddf389b4b32444ff5d2d6ab2de2b994ffd39caaf
Arun tripathy <139703585+Devopsarun1234@users.noreply.github.com>
2025-04-19 16:49:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI ID (for us-east-1)
+  ami           = "ami-08bf489a05e916bbd" # Amazon Linux 2 AMI ID (for us-east-1)
   instance_type = var.instance_type
 
   tags = {
